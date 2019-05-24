9d017b451d2a102c49a75cfaf3f78a4aae91b633
Marcos Vinícius Rubido <marcosauda@hotmail.com>
2019-05-24 07:07:07 +0200

    Update convertToPlant.ts

--- src/convertToPlant.ts
+++ src/convertToPlant.ts
@@ -12,21 +12,21 @@ const USAGE_LEFT_ARROW: string = '<--';
 export function convertToPlant(gtmJSON: IGTMWorkspace): string {
     const plantDocument: UniqueValueArray<string> = new UniqueValueArray<string>('@startuml');
     gtmJSON.containerVersion.variable.forEach((variable: IGTMVariable): void => {
+        plantDocument.push(`object "${variable.name}"`);
         gtmJSON.containerVersion.tag.forEach((tag: IGTMTag): void => {
             plantDocument.push(`object "${tag.name}"`);
-            if (tag.parameter.map((parameter: IGTMParameter): string => parameter.value).indexOf(variable.name)) {
+            if (tag.parameter.some((parameter: IGTMParameter): boolean => parameter.value.indexOf(variable.name) >= 0)) {
                 plantDocument.push(`"${variable.name}" ${USAGE_LEFT_ARROW} "${tag.name}"`);
             }
         });
         gtmJSON.containerVersion.trigger.forEach((trigger: IGTMTrigger): void => {
             plantDocument.push(`object "${trigger.name}"`);
-            if (trigger.customEventFilter !== undefined && trigger.customEventFilter.some((customEventFilter: IGTMCustomEventFilter): boolean => !!customEventFilter.parameter.map((parameter: IGTMParameter): string => parameter.value).indexOf(variable.name))) {
+            if (trigger.customEventFilter !== undefined && trigger.customEventFilter.some((customEventFilter: IGTMCustomEventFilter): boolean => customEventFilter.parameter.some((parameter: IGTMParameter): string => parameter.value.indexOf(variable.name) >= 0))) {
                 plantDocument.push(`"${variable.name}" ${USAGE_LEFT_ARROW} "${trigger.name}"`);
             }
             plantDocument.push(`object "${trigger.name}"`);
         });
-        plantDocument.push(`object "${variable.name}"`);
     });
     plantDocument.push('@enduml')
-    return plantDocument.filter((value: string, index: number, array: string[]): boolean => array.indexOf(value) === index).join(os.EOL);
+    return plantDocument.join(os.EOL);
 }
